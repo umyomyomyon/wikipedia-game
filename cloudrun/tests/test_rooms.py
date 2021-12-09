2967f42ab6d58967baf0294918fb388685f0012b
umyomyomyon <minecraft.sentry.yym@gmail.com>
2021-12-10 00:53:43 +0900

    部屋参加API作成, テスト作成

--- cloudrun/tests/test_rooms.py
+++ cloudrun/tests/test_rooms.py
@@ -1,6 +1,8 @@
+import pytest
 from firebase_admin import db
 
-from rooms import check_room_exists, init_room, destroy_room
+from rooms import check_room_exists, init_room, destroy_room, _join_room
+from exceptions import RoomNotExistException
 
 
 #  init_room -> destroy_roomの順番でテストを実行すること
@@ -10,10 +12,9 @@ def test_init_room():
     user_name = 'test_user_name'
     expected_data = {
         'isReady': False,
-        'users': [{
-            'name': user_name,
-            'uuid': user_uuid
-        }]
+        'users': {
+            user_uuid: user_name
+        }
     }
 
     init_room(room_id, user_uuid, user_name)
@@ -32,20 +33,48 @@ def test_destroy_room():
     assert not bool(ref.get())
 
 
-def room_decorator(f):
-    room_id = 10000
-    user_uuid = 'test_user_uuid'
-    user_name = 'test_user_name'
-
-    def _wrapper(*args, **kwargs):
-        init_room(room_id, user_uuid, user_name)
-        f(*args, **kwargs)
+def test_destroy_room_failed():
+    with pytest.raises(RoomNotExistException):
+        room_id = 22345
         destroy_room(room_id)
-    return _wrapper
 
 
-@room_decorator
+def room_decorator(room_id):
+    def _room_decorator(f):
+        _room_id = 10000 if room_id is None else room_id
+        user_uuid = 'test_user_uuid'
+        user_name = 'test_user_name'
+
+        def _wrapper(*args, **kwargs):
+            init_room(_room_id, user_uuid, user_name)
+            f(*args, **kwargs)
+            destroy_room(_room_id)
+        return _wrapper
+    return _room_decorator
+
+
+@room_decorator(10000)
 def test_check_room_exists():
     room_id = 10000
     result = check_room_exists(room_id)
     assert result is True
+
+
+@room_decorator(20000)
+def test_join_room():
+    room_id = 20000
+    user_uuid = 'test_user_uuid'
+    user_name = 'test_user_name'
+    _join_room(room_id, user_uuid, user_name)
+
+    ref = db.reference(f'{room_id}/users/{user_uuid}/')
+    registered_user_name = ref.get()
+    assert registered_user_name == user_name
+
+
+def test_join_room_failed():
+    with pytest.raises(RoomNotExistException):
+        room_id = 20001
+        user_uuid = 'test_user_uuid'
+        user_name = 'test_user_name'
+        _join_room(room_id, user_uuid, user_name)
